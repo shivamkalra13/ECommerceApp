3e6cbe3b63812fd5910de4e76fb7e4eab37b20f6
shivamkalra13 <shivamkalra13@gmail.com>
2020-01-27 02:55:02 +0530

    main activity ui completed partially

--- app/src/main/java/d/androidapps/ecommerceapp/MainActivity.java
+++ app/src/main/java/d/androidapps/ecommerceapp/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar=findViewById(R.id.custtoolbar);
-        setSupportActionBar(toolbar);
+
 
 
     }
