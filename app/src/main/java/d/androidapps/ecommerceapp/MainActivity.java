119585bebe9ae5266ad9d6284c82ed79470166b2
Srijan Biswas <srijanbiswas3@gmail.com>
2020-02-01 00:37:20 +0530

    logo changed

--- app/src/main/java/d/androidapps/ecommerceapp/MainActivity.java
+++ app/src/main/java/d/androidapps/ecommerceapp/MainActivity.java
@@ -3,15 +3,34 @@ package d.androidapps.ecommerceapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button login,signup,google;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login=findViewById(R.id.login);
+        signup=findViewById(R.id.signup);
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,SignupActivity.class);
+                startActivity(i);
+            }
+        });
 
 
     }
