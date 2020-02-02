3e94572d199149296d75b3ff377ed83360ac6edf
shivamkalra13 <shivamkalra13@gmail.com>
2020-02-03 02:25:13 +0530

    on click animation for buttons

--- app/src/main/java/d/androidapps/ecommerceapp/MainActivity.java
+++ app/src/main/java/d/androidapps/ecommerceapp/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
